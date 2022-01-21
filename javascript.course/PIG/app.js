099c9ed109782f308db98052919fd9eb4ed1e699
Bev B <gniniartnsawti@gmail.com>
2022-01-21 15:17:33 -0500

    Update app.js

--- javascript.course/PIG/app.js
+++ javascript.course/PIG/app.js
@@ -11,14 +11,21 @@ GAME RULES:
 
 // Selecting elements
 
-const score0El = document.querySelector('#score--0')
-const score1El = document.getElementById('score--1')
+const score0El = document.querySelector('#score-0')
+const score1El = document.getElementById('score-1')
 const diceEl = document.querySelector('.dice')
-const btnNew = document.querySelector('.btn--new')
-const btnRoll = document.querySelector('.btn--roll')
-const btnHold = document.querySelector('.btn--hold')
+const btnNew = document.querySelector('.btn-new')
+const btnRoll = document.querySelector('.btn-roll')
+const btnHold = document.querySelector('.btn-hold')
 
 //Starting conditions
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
+
+//Rolling dice function
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1
+  diceEl.classList.remove('hidden')
+  diceEl.src = `dice-${dice}.png`
+})
