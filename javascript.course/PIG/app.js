c718397f75d85b871421ebb300a79b784a1981cb
Bev B <gniniartnsawti@gmail.com>
2022-01-21 14:40:59 -0500

    ****video1

--- javascript.course/PIG/app.js
+++ javascript.course/PIG/app.js
@@ -13,6 +13,9 @@ GAME RULES:
 
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')
+const diceEl = document.querySelector('.dice')
 
+//Starting conditions
 score0El.textContent = 0
 score1El.textContent = 0
+diceEl.classList.add('hidden')
