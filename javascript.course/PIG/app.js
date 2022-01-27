d826a1236876b8af080b689d053fb07206cdd092
Bev B <gniniartnsawti@gmail.com>
2022-01-27 15:15:52 -0500

    Update app.js

--- javascript.course/PIG/app.js
+++ javascript.course/PIG/app.js
@@ -36,11 +36,14 @@ btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1
   diceEl.classList.remove('hidden')
   diceEl.src = `dice-${dice}.png`
+
   if (dice !== 1) {
     currentScore += dice
     document.getElementById(`current-${activePlayer}`).textContent =
       currentScore
   } else {
+    document.getElementById(`current-${activePlayer}`).textContent = 0
     activePlayer = activePlayer === 0 ? 1 : 0
+    currentScore = 0
   }
 })
