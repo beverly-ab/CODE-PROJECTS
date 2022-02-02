1c83843e3efe7a874a86ec4fd18022df79739190
Bev B <gniniartnsawti@gmail.com>
2022-02-02 16:56:41 -0500

    Update script.js

--- javascript.course/modal/script.js
+++ javascript.course/modal/script.js
@@ -3,9 +3,17 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button clicked!');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
